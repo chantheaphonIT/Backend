fff3ac80e9ecbae2ee58eb067f03f25dc7253d35
chantheaphonIT <chanthea.phon@student.passerellesnumeriques.org>
2021-11-05 11:38:45 +0700

    commit backend

---
+++ app/Http/Controllers/BookController.php
@@ -0,0 +1,82 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class BookController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+        return Book:: get();
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+        $book = new Book();
+       $book->author = $request->author_id;
+       $book->title = $request->title;
+       $book->body = $request->body;
+
+       $book->save();
+       return response()->json(['sms' => 'Book are created'],201);
+
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+        return Book::findOrFail($id);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        $request->validate([
+            'title'=>'min:3|max:10',
+            'body'=>'min:3|max:50',
+        ]);
+        $request->save();
+
+        return response()-> json(['sms'=> 'updated'], 200);
+
+
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+        return Book::findOrFail($id);
+    }
+}
